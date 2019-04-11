f1e181d93de63e4cc5613be18c499e7fe17194f4
huangweibiao <huangweibiao@instoms.cn>
2019-04-11 14:13:07 +0800

    add

---
+++ service-erp-core/src/main/java/com/seamwhole/serviceerpcore/ServiceErpCoreApplication.java
@@ -0,0 +1,13 @@
+package com.seamwhole.serviceerpcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceErpCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceErpCoreApplication.class, args);
+    }
+
+}
