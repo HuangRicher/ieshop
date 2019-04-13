6352e63dc4e7c3d8f95e3062e08428ae476b5fae
huangweibiao <huangweibiao@instoms.cn>
2019-04-13 18:15:49 +0800

    add

---
+++ web-erp-admin/src/main/java/com/seamwhole/weberpadmin/WebErpAdminApplication.java
@@ -0,0 +1,13 @@
+package com.seamwhole.weberpadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebErpAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebErpAdminApplication.class, args);
+    }
+
+}
